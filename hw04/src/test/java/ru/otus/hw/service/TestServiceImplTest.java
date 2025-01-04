785450a5cef74c4783a7eaa25774c9f3d96d6bd7
JSkuns <studio.domi@yandex.ru>
2025-01-04 18:20:42 +0300

    Первое решение четвёртого ДЗ

---
+++ hw04/src/test/java/ru/otus/hw/service/TestServiceImplTest.java
@@ -0,0 +1,47 @@
+package ru.otus.hw.service;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import ru.otus.hw.dao.QuestionDao;
+import ru.otus.hw.domain.Answer;
+import ru.otus.hw.domain.Question;
+import ru.otus.hw.domain.Student;
+import ru.otus.hw.domain.TestResult;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class TestServiceImplTest {
+
+    private TestServiceImpl service;
+    private Student student;
+    private Question question;
+
+    @BeforeEach
+    void setup() {
+        student = new Student("John", "Doe");
+        List<Answer> answersList = List.of(
+                new Answer("answer1", true),
+                new Answer("answer2", false)
+        );
+        question = new Question("question1", answersList);
+
+        LocalizedIOService ioServiceMock = Mockito.mock(LocalizedIOService.class);
+        QuestionDao daoMock = Mockito.mock(QuestionDao.class);
+
+        service = new TestServiceImpl(ioServiceMock, daoMock);
+    }
+
+    @Test
+    void testExpectedCorrectAnswer() {
+        // Act
+        TestResult result = service.executeTestFor(student);
+        result.applyAnswer(question, true);
+
+        // Assert
+        assertEquals(1, result.getRightAnswersCount());
+    }
+
+}
