be26f03c053e2b827b6e7426cce96ba527580b04
VVTerekhin <vterekhin@cbgr.ru>
2025-01-09 11:23:31 +0300

    Вторые исправления в четвёртом ДЗ. Согласно замечаниям.

--- hw04/src/main/java/ru/otus/hw/shell/ApplicationCommands.java
+++ hw04/src/main/java/ru/otus/hw/shell/ApplicationCommands.java
@@ -1,35 +1,39 @@
 package ru.otus.hw.shell;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.shell.ExitRequest;
-import org.springframework.shell.standard.ShellComponent;
-import org.springframework.shell.standard.ShellMethod;
+import org.springframework.context.ApplicationContext;
+import org.springframework.shell.command.annotation.Command;
 import org.springframework.shell.standard.commands.Quit;
 import ru.otus.hw.service.LocalizedIOService;
+import ru.otus.hw.service.TestRunnerService;
 
-@ShellComponent(value = "Applications Shell Commands")
+@Command(group = "Applications Shell Commands")
 public class ApplicationCommands implements Quit.Command {
 
     private final LocalizedIOService ioService;
 
+    private final ApplicationContext context;
+
     @Autowired
-    public ApplicationCommands(LocalizedIOService ioService) {
+    public ApplicationCommands(LocalizedIOService ioService, ApplicationContext context) {
         this.ioService = ioService;
+        this.context = context;
     }
 
     /**
      * При вводе команды в shell - запустится основное тестирование
      */
-    @ShellMethod(value = "Run application.", key = {"r", "run"})
+    @Command(alias = "r", command = "run", description = "Run application.")
     public void runApp() {
         ioService.printLineLocalized("ApplicationCommands.starting.testing");
-        throw new ExitRequest();
+        var testRunnerService = context.getBean(TestRunnerService.class);
+        testRunnerService.run();
     }
 
     /**
      * При вводе команды в shell - будет выполнен выход из программы
      */
-    @ShellMethod(value = "Exit the application.", key = {"q", "e", "quit", "exit"})
+    @Command(alias = {"q", "e"}, command = {"quit", "exit"}, description = "Exit the application.")
     public void quit() {
         ioService.printLineLocalized("ApplicationCommands.exit.testing.program");
         System.exit(0);
