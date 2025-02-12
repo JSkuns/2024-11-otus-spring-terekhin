e1564fd6046f7cf918232a54c77fe884e62bf379
VVTerekhin <vterekhin@cbgr.ru>
2025-02-12 08:13:41 +0300

    Пробую добавить flyway

--- hw09/src/main/java/ru/otus/hw/Application.java
+++ hw09/src/main/java/ru/otus/hw/Application.java
@@ -6,6 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Application {
 
+	/**
+	 * <a href="http://localhost:8080/">...</a>
+	 */
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
---
+++ hw09/src/main/resources/db.migration/V2/V2_1__create_table_authors.sql
@@ -0,0 +1,6 @@
+CREATE TABLE IF NOT EXISTS authors
+(
+    id BIGINT PRIMARY KEY,
+--     id BIGSERIAL PRIMARY KEY,
+    fullName VARCHAR(255)
+);
